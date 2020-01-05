59278e30a00ef853f2929b8a4f2d9b3a454d5cd4
Alexander <alexanderamirez002@gmail.com>
2020-01-05 09:18:18 -0400

    Babel had problems with the transform runtime plugin, now is ok. News Api called Works Ok

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 import React, {Component} from 'react';
 
 class App extends Component {
-    state = {   }
+
+    componentDidMount() {
+        this.callNewsApi();
+    }
+
+    callNewsApi = async () => {
+
+        const url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=bb59d49fde4c44a3b98ec85aab089ea4`;
+
+        const response = await fetch(url);
+        const news = await response.json();
+
+        console.log(news);
+    }
+    
     render() { 
         return ( 
             <div className="container">
